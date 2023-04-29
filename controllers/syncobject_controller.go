d9fcf43ccc5894692cb5d4639fa2b1e777da6c83
Simon Jürgensmeyer <Simon.Juergensmeyer@gmail.com>
2023-04-29 13:30:13 +0200

    Add Finalizer

--- controllers/syncobject_controller.go
+++ controllers/syncobject_controller.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"context"
+	"errors"
 	"fmt"
 
 	syncv1alpha1 "github.com/sj14/sync-operator/api/v1alpha1"
@@ -13,6 +14,7 @@ import (
 	"k8s.io/apimachinery/pkg/types"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 	"sigs.k8s.io/controller-runtime/pkg/log"
 )
 
@@ -22,16 +24,14 @@ type SyncObjectReconciler struct {
 	Scheme *runtime.Scheme
 }
 
+const finalizerName = "sync.sj14.github.io/finalizer"
+
 //+kubebuilder:rbac:groups=sync.sj14.github.io,resources=syncobjects,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=sync.sj14.github.io,resources=syncobjects/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=sync.sj14.github.io,resources=syncobjects/finalizers,verbs=update
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
-// TODO(user): Modify the Reconcile function to compare the state specified by
-// the SyncObject object against the actual cluster state, and then
-// perform operations to make the cluster state reflect the state specified by
-// the user.
 //
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.14.1/pkg/reconcile
@@ -42,11 +42,23 @@ func (r *SyncObjectReconciler) Reconcile(ctx context.Context, req ctrl.Request) 
 
 	var syncObject syncv1alpha1.SyncObject
 
-	if err := r.Client.Get(ctx, req.NamespacedName, &syncObject); err != nil {
+	err := r.Client.Get(ctx, req.NamespacedName, &syncObject)
+	if apierrors.IsNotFound(err) {
+		return ctrl.Result{}, nil
+	}
+	if err != nil {
 		return ctrl.Result{}, fmt.Errorf("failed getting SyncObject: %v", err)
 	}
 
 	logger.Info("reference", "reference", syncObject.Spec.Reference)
+
+	stop, err := r.handleFinalizer(ctx, &syncObject)
+	if err != nil {
+		return ctrl.Result{}, fmt.Errorf("failed handling finalizer: %v", err)
+	}
+	if stop {
+		return ctrl.Result{}, nil
+	}
 
 	var original unstructured.Unstructured
 	original.SetGroupVersionKind(schema.GroupVersionKind{
@@ -69,7 +81,50 @@ func (r *SyncObjectReconciler) Reconcile(ctx context.Context, req ctrl.Request) 
 		r.replicate(ctx, original, namespace)
 	}
 
+	// TODO: check if there are replicaes which need to be removed, e.g. because a targetNamespace got removed or a ignoreNamespace got added.
+
 	return ctrl.Result{}, nil
+}
+
+func (r *SyncObjectReconciler) handleFinalizer(ctx context.Context, syncObject *syncv1alpha1.SyncObject) (stop bool, err error) {
+	if syncObject.Spec.DisableFinalizer {
+		return false, nil
+	}
+
+	// examine DeletionTimestamp to determine if object is under deletion
+	if syncObject.ObjectMeta.DeletionTimestamp.IsZero() {
+		// The object is not being deleted, so if it does not have our finalizer,
+		// then lets add the finalizer and update the object. This is equivalent
+		// registering our finalizer.
+		if !controllerutil.ContainsFinalizer(syncObject, finalizerName) {
+			controllerutil.AddFinalizer(syncObject, finalizerName)
+			if err := r.Update(ctx, syncObject); err != nil {
+				return true, err
+			}
+		}
+		return false, nil
+	}
+
+	// The object is being deleted
+	if controllerutil.ContainsFinalizer(syncObject, finalizerName) {
+		// our finalizer is present, so lets handle any external dependency
+		if err := r.deleteAllReplicas(ctx, *syncObject); err != nil {
+			// if fail to delete the external dependency here, return with error
+			// so that it can be retried
+			return true, err
+		}
+
+		// remove our finalizer from the list and update it.
+		controllerutil.RemoveFinalizer(syncObject, finalizerName)
+		if err := r.Update(ctx, syncObject); err != nil {
+			return true, err
+		}
+
+		// Stop reconciliation as the item is being deleted
+		return true, nil
+	}
+
+	return false, nil
 }
 
 // SetupWithManager sets up the controller with the Manager.
@@ -130,14 +185,13 @@ func (r *SyncObjectReconciler) replicate(ctx context.Context, original unstructu
 	// TODO: add more?
 
 	// create new replica if it doesn't already exist
-	var err error
-	if err = r.Client.Create(ctx, replica); client.IgnoreAlreadyExists(err) != nil {
-		return fmt.Errorf("failed creating replica in %q: %v", namespace, err)
-	}
-
+	err := r.Client.Create(ctx, replica)
 	if !apierrors.IsAlreadyExists(err) {
 		// we create a new replica, no need for updating it
 		return nil
+	}
+	if err != nil {
+		return fmt.Errorf("failed creating replica in %q: %v", namespace, err)
 	}
 
 	// replica already exists, just update it
@@ -146,4 +200,47 @@ func (r *SyncObjectReconciler) replicate(ctx context.Context, original unstructu
 	}
 
 	return nil
+}
+
+// TODO: check ownerreference or something before deleting
+func (r *SyncObjectReconciler) deleteReplica(ctx context.Context, syncObject syncv1alpha1.SyncObject, namespace string) error {
+	var objectToDelete unstructured.Unstructured
+	objectToDelete.SetName(syncObject.Spec.Reference.Name)
+	objectToDelete.SetNamespace(namespace)
+	objectToDelete.SetGroupVersionKind(schema.GroupVersionKind{
+		Group:   syncObject.Spec.Reference.Group,
+		Version: syncObject.Spec.Reference.Version,
+		Kind:    syncObject.Spec.Reference.Kind,
+	})
+
+	log.Log.Info("going to delete", "object", objectToDelete, "ns", namespace)
+
+	if err := r.Client.Delete(ctx, &objectToDelete); err != nil {
+		return fmt.Errorf("failed deleting replica: %v", err)
+	}
+
+	return nil
+}
+
+func (r *SyncObjectReconciler) deleteAllReplicas(ctx context.Context, syncObject syncv1alpha1.SyncObject) error {
+	var namespaces corev1.NamespaceList
+
+	if err := r.Client.List(ctx, &namespaces); err != nil {
+		return fmt.Errorf("failed listing namespaces: %v", err)
+	}
+
+	var err error
+	for _, namespace := range namespaces.Items {
+		if namespace.GetName() == syncObject.Spec.Reference.Namespace {
+			// do not delete the original
+			continue
+		}
+
+		// log.Log.Info("going to delete", "object", objectToDelete, "ns", namespace.GetName())
+		if err := r.deleteReplica(ctx, syncObject, namespace.GetName()); err != nil {
+			errors.Join(err)
+		}
+	}
+
+	return err
 }
