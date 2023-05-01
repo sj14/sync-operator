1adcb6f250bf1c75799007db84582234e57976dc
Simon Jürgensmeyer <Simon.Juergensmeyer@gmail.com>
2023-05-01 19:10:54 +0200

    Minor formatting

--- main.go
+++ main.go
@@ -30,14 +30,12 @@ func init() {
 }
 
 func main() {
-	var metricsAddr string
-	var enableLeaderElection bool
-	var probeAddr string
-	flag.StringVar(&metricsAddr, "metrics-bind-address", ":8080", "The address the metric endpoint binds to.")
-	flag.StringVar(&probeAddr, "health-probe-bind-address", ":8081", "The address the probe endpoint binds to.")
-	flag.BoolVar(&enableLeaderElection, "leader-elect", false,
-		"Enable leader election for controller manager. "+
-			"Enabling this will ensure there is only one active controller manager.")
+	var (
+		metricsAddr          = flag.String("metrics-bind-address", ":8080", "The address the metric endpoint binds to.")
+		probeAddr            = flag.String("health-probe-bind-address", ":8081", "The address the probe endpoint binds to.")
+		enableLeaderElection = flag.Bool("leader-elect", false, "Enable leader election for controller manager. Enabling this will ensure there is only one active controller manager.")
+	)
+
 	opts := zap.Options{
 		Development: true,
 	}
@@ -48,10 +46,10 @@ func main() {
 
 	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
 		Scheme:                 scheme,
-		MetricsBindAddress:     metricsAddr,
+		MetricsBindAddress:     *metricsAddr,
 		Port:                   9443,
-		HealthProbeBindAddress: probeAddr,
-		LeaderElection:         enableLeaderElection,
+		HealthProbeBindAddress: *probeAddr,
+		LeaderElection:         *enableLeaderElection,
 		LeaderElectionID:       "e02338ab.sync-operator.sj14.github.io",
 		// LeaderElectionReleaseOnCancel defines if the leader should step down voluntarily
 		// when the Manager ends. This requires the binary to immediately end when the
