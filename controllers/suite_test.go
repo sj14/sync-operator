a07abd727f85944c5b39cb757faf748ac34ae4a7
Simon Jürgensmeyer <Simon.Juergensmeyer@gmail.com>
2023-05-01 20:12:13 +0200

    Add basic suite test

--- controllers/suite_test.go
+++ controllers/suite_test.go
@@ -1,61 +1,201 @@
 package controllers
 
 import (
+	"context"
+	"log"
+	"os"
 	"path/filepath"
 	"testing"
+	"time"
 
-	. "github.com/onsi/ginkgo/v2"
-	. "github.com/onsi/gomega"
-
+	syncv1alpha1 "github.com/sj14/sync-operator/api/v1alpha1"
+	"github.com/stretchr/testify/require"
+	corev1 "k8s.io/api/core/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes/scheme"
-	"k8s.io/client-go/rest"
+	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/envtest"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
-
-	syncv1alpha1 "github.com/sj14/sync-operator/api/v1alpha1"
 )
 
-// These tests use Ginkgo (BDD-style Go testing framework). Refer to
-// http://onsi.github.io/ginkgo/ to learn more about Ginkgo.
+var (
+	k8sClient client.Client
+)
 
-var cfg *rest.Config
-var k8sClient client.Client
-var testEnv *envtest.Environment
+func TestMain(m *testing.M) {
+	logf.SetLogger(zap.New(zap.WriteTo(os.Stdout), zap.UseDevMode(true)))
+	ctx, cancel := context.WithCancel(context.TODO())
 
-func TestAPIs(t *testing.T) {
-	RegisterFailHandler(Fail)
-
-	RunSpecs(t, "Controller Suite")
-}
-
-var _ = BeforeSuite(func() {
-	logf.SetLogger(zap.New(zap.WriteTo(GinkgoWriter), zap.UseDevMode(true)))
-
-	By("bootstrapping test environment")
-	testEnv = &envtest.Environment{
-		CRDDirectoryPaths:     []string{filepath.Join("..", "config", "crd", "bases")},
+	testEnv := &envtest.Environment{
+		CRDDirectoryPaths:     []string{filepath.Join("..", "deploy", "crds")},
 		ErrorIfCRDPathMissing: true,
 	}
 
-	var err error
-	// cfg is defined in this file globally.
-	cfg, err = testEnv.Start()
-	Expect(err).NotTo(HaveOccurred())
-	Expect(cfg).NotTo(BeNil())
+	cfg, err := testEnv.Start()
+	if err != nil {
+		log.Panicf("failed starting the test environment: %s\n", err)
+	}
+	if cfg == nil {
+		log.Panicln("testenv config is nil")
+	}
+	defer func() {
+		log.Println("tearing down the test environment")
+		cancel()
+		if err := testEnv.Stop(); err != nil {
+			log.Printf("failed stopping test environment: %s\n", err)
+		}
+	}()
 
 	err = syncv1alpha1.AddToScheme(scheme.Scheme)
-	Expect(err).NotTo(HaveOccurred())
+	if err != nil {
+		log.Panicf("failed adding scheme: %s\n", err)
+	}
 
 	k8sClient, err = client.New(cfg, client.Options{Scheme: scheme.Scheme})
-	Expect(err).NotTo(HaveOccurred())
-	Expect(k8sClient).NotTo(BeNil())
+	if err != nil {
+		log.Panicf("failed creating new controller client: %s\n", err)
+	}
+	if k8sClient == nil {
+		log.Panicln("k8sClient is nil")
+	}
 
-})
+	k8sManager, err := ctrl.NewManager(cfg, ctrl.Options{
+		Scheme: scheme.Scheme,
+	})
+	if err != nil {
+		log.Panicf("failed creating new manager: %s\n", err)
+	}
 
-var _ = AfterSuite(func() {
-	By("tearing down the test environment")
-	err := testEnv.Stop()
-	Expect(err).NotTo(HaveOccurred())
-})
+	err = (&SyncObjectReconciler{
+		Client: k8sManager.GetClient(),
+		Scheme: k8sManager.GetScheme(),
+	}).SetupWithManager(k8sManager)
+	if err != nil {
+		log.Panicf("SyncObjectReconciler setup failed: %s\n", err)
+	}
+
+	go func() {
+		err = k8sManager.Start(ctx)
+		if err != nil {
+			log.Panicf("failed starting k8s manager: %s\n", err)
+		}
+	}()
+
+	os.Exit(m.Run())
+}
+
+const (
+	timeout  = 10 * time.Second
+	interval = 250 * time.Millisecond
+)
+
+func TestControllersCreateDelete(t *testing.T) {
+	ctx := context.Background()
+
+	targetNamespace := &corev1.Namespace{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "ConfigMap",
+			APIVersion: "v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "target-namespace",
+		},
+	}
+
+	configMapPayload := map[string]string{
+		"key1": "value1",
+		"key2": "value2",
+		"key3": "value3",
+	}
+
+	require.NoError(t, k8sClient.Create(ctx, getOriginNamespace()))
+	require.NoError(t, k8sClient.Create(ctx, getOriginConfigMap(configMapPayload)))
+	require.NoError(t, k8sClient.Create(ctx, targetNamespace))
+
+	// just for comparison, do not create target configmap
+	targetConfigMap := &corev1.ConfigMap{
+		TypeMeta: metav1.TypeMeta{
+			APIVersion: "v1",
+			Kind:       "ConfigMap",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      getOriginConfigMap(nil).Name, // we keep the name from the origin
+			Namespace: targetNamespace.Name,
+		},
+	}
+
+	t.Run("Check if target namespace does not contain replica", func(t *testing.T) {
+		err := k8sClient.Get(ctx, client.ObjectKeyFromObject(targetConfigMap), targetConfigMap)
+		require.True(t, apierrors.IsNotFound(err))
+	})
+
+	t.Run("Create SyncObject", func(t *testing.T) {
+		syncObject := &syncv1alpha1.SyncObject{
+			TypeMeta: metav1.TypeMeta{
+				APIVersion: "sync.sj14.github.io/v1alpha1",
+				Kind:       "SyncObject",
+			},
+			ObjectMeta: metav1.ObjectMeta{
+				Name: "sync-test",
+			},
+			Spec: syncv1alpha1.SyncObjectSpec{
+				Reference: syncv1alpha1.Reference{
+					Group:     "",
+					Version:   "v1",
+					Kind:      getOriginConfigMap(nil).Kind,
+					Name:      getOriginConfigMap(nil).Name,
+					Namespace: getOriginConfigMap(nil).Namespace,
+				},
+			},
+		}
+		require.NoError(t, k8sClient.Create(ctx, syncObject))
+	})
+
+	t.Run("Check replica", func(t *testing.T) {
+		// be sure that we didn't already get the replica by accident by checking the data
+		require.Equal(t, map[string]string(map[string]string(nil)), targetConfigMap.Data)
+
+		// get the replica
+		require.Eventually(t, func() bool {
+			if err := k8sClient.Get(ctx, client.ObjectKeyFromObject(targetConfigMap), targetConfigMap); err != nil {
+				log.Println(err)
+				return false
+			}
+			return true
+		}, timeout, interval)
+
+		// check that the data was synced succesfully
+		require.Equal(t, configMapPayload, targetConfigMap.Data)
+	})
+}
+
+// helper as gvk would be missing after creation
+func getOriginNamespace() *corev1.Namespace {
+	return &corev1.Namespace{
+		TypeMeta: metav1.TypeMeta{
+			APIVersion: "v1",
+			Kind:       "ConfigMap",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "origin-namespace",
+		},
+	}
+}
+
+// helper as gvk would be missing after creation
+func getOriginConfigMap(payload map[string]string) *corev1.ConfigMap {
+	return &corev1.ConfigMap{
+		TypeMeta: metav1.TypeMeta{
+			APIVersion: "v1",
+			Kind:       "ConfigMap",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "origin-configmap",
+			Namespace: getOriginNamespace().Name,
+		},
+		Data: payload,
+	}
+}
